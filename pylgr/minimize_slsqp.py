fixed sign of kkt multipliers

pylgr/minimize_slsqp.py
@@ -399,7 +399,7 @@ def _minimize_slsqp(
     il = im + la
     ix = il + (n1*n)//2 + 1
     ir = ix + n - 1
-    _kkt_mult = np.abs(w[ir: ir + m])
+    _kkt_mult = w[ir:ir + m]
 
     # KKT multipliers
     w_ind = 0

pylgr/solve.py
@@ -42,7 +42,7 @@ class DirectSolution:
 
         # Extract KKT multipliers and use to approximate costates
         self.dVdX = NLP_res.kkt['eq'][0].reshape(self.X.shape, order=order)
-        self.dVdX = self.dVdX * -np.sign(dXdt) / w.reshape(1,-1)
+        self.dVdX = self.dVdX / w.reshape(1,-1)
 
         self.sol_X = LagrangeInterpolator(tau, self.X)
         self.sol_U = LagrangeInterpolator(tau, self.U, U_lb, U_ub)